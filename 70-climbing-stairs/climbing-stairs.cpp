a131609504566643f249580d0420b593bf3553f4
KhyatiKakkar03 <149072568+KhyatiKakkar03@users.noreply.github.com>
2025-05-15 11:22:02 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (34.02%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,15 +1,21 @@
 class Solution {
 public:
-    int cs(int n,vector<int> &dp)
-    {
-        if(n<=1)
-        return 1;
-        if(dp[n]!=-1)
-        return dp[n];
-        return dp[n]=cs(n-1,dp)+cs(n-2,dp);
-    }
+    // int cs(int n,vector<int> &dp)
+    // {
+    //     if(n<=1)
+    //     return 1;
+    //     if(dp[n]!=-1)
+    //     return dp[n];
+    //     return dp[n]=cs(n-1,dp)+cs(n-2,dp);
+    // }
     int climbStairs(int n) {
         vector<int> dp(n+1,-1);
-        return cs(n,dp);
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<=n;i++)
+        {
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 };
